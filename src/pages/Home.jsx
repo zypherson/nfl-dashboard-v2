48fe8b1d656cc82857e02da04f7f2d88c1f73887
zypherson <taylorzypherson@gmail.com>
2026-05-08 01:37:48 -0400

    fetched the teams to the home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { getTeams } from "../services/nfl";
+import { Link } from "react-router-dom";
+
 const Home = () => {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const data = await getTeams();
+      setTeams(data);
+    };
+
+    fetchTeams();
+  }, []);
+
   return (
     <div>
       <h1>NFL Teams</h1>
+
+      {teams.map(team => (
+        <Link key={team.team.id} to={`/team/${team.team.id}`}>
+          <p>{team.team.name}</p>
+        </Link>
+      ))}
     </div>
   );
 };
