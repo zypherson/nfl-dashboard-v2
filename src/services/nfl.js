b920727a0d0ea8c3bfb9a015cd04ec0cb7963e23
zypherson <taylorzypherson@gmail.com>
2026-05-09 00:59:12 -0400

    added api calls to get playes & teams by ID

--- src/services/nfl.js
+++ src/services/nfl.js
@@ -9,3 +9,23 @@ export const getTeams = async () => {
     return [];
   }
 };
+
+export const getTeamById = async (id) => {
+  try {
+    const res = await api.get(`/teams?id=${id}`);
+    return res.data.response[0];
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+export const getPlayersByTeam = async (teamId) => {
+  try {
+    const res = await api.get(`/players?team=${teamId}&season=2023`);
+    return res.data.response;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
