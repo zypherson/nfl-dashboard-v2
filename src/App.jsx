9e1023d7324d59999fc8c59402dc9ab840fc58e2
zypherson <taylorzypherson@gmail.com>
2026-05-23 01:18:46 -0400

    added route for favoites in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TeamDetails from "./pages/TeamDetails";
 import PlayerDetails from "./pages/PlayerDetails";
+import Favorites from "./pages/Favorites";
+
 function App() {
   return (
     <Router>
@@ -9,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/team/:id" element={<TeamDetails />} />
         <Route path="/player/:id" element={<PlayerDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </Router>
   );
