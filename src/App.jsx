4d7e371292dd0a80d2fa28a42d4c16bb270e07dc
zypherson <taylorzypherson@gmail.com>
2026-05-29 02:37:52 -0400

    added navbar to app jsx along with the routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import Home from "./pages/Home";
 import TeamDetails from "./pages/TeamDetails";
 import PlayerDetails from "./pages/PlayerDetails";
 import Favorites from "./pages/Favorites";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <Router>
+
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/team/:id" element={<TeamDetails />} />
         <Route path="/player/:id" element={<PlayerDetails />} />
+        <Route path="/compare" element={<ComparePlayers />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
+
     </Router>
   );
 }
-
 export default App;
