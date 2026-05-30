4b1a518cf2e70adf17055be657da577d557910d3
zypherson <taylorzypherson@gmail.com>
2026-05-30 11:51:42 -0400

    added some basic styling to players ssearch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,13 +23,20 @@ const Home = () => {
   return (
     <div>
       <h1>NFL Teams</h1>
-      <input
-          type="text"
-          placeholder="Search NFL teams..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      {teams.map(team => (
+     <input
+  type="text"
+  placeholder="Search NFL teams..."
+  value={searchTerm}
+  onChange={(e) => setSearchTerm(e.target.value)}
+  style={{
+    padding: "0.75rem",
+    width: "300px",
+    marginBottom: "1rem",
+    borderRadius: "8px",
+    border: "1px solid #ccc"
+  }}
+/>
+      {filteredTeams.map(team => (
         <Link key={team.team.id} to={`/team/${team.team.id}`}>
           <p>{team.team.name}</p>
         </Link>
